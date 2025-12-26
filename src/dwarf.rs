294a006ec30647c3b1c6008ea75f64cf8dcdb6ae
lkitching <lee.kitching@gmail.com>
2025-12-26 16:09:09 +0000

    Index functions by name in Dwarf type
    Add function index to the Dwarf type which maps names to a collection
    of DIEs with that name.

--- src/dwarf.rs
+++ src/dwarf.rs
@@ -10,6 +10,75 @@ use strum_macros::FromRepr;
 use crate::elf::{Elf};
 use crate::types::{FileAddress, TryFromBytes};
 use crate::error::Error;
+use crate::multimap::UnorderedMultiMap;
+
+#[allow(non_camel_case_types)]
+#[repr(u64)]
+#[derive(Copy, Clone, PartialEq, Eq, Debug, FromRepr)]
+pub enum DwarfTag {
+    DW_TAG_array_type = 0x01,
+    DW_TAG_class_type = 0x02,
+    DW_TAG_entry_point = 0x03,
+    DW_TAG_enumeration_type = 0x04,
+    DW_TAG_formal_parameter = 0x05,
+    DW_TAG_imported_declaration = 0x08,
+    DW_TAG_label = 0x0a,
+    DW_TAG_lexical_block = 0x0b,
+    DW_TAG_member = 0x0d,
+    DW_TAG_pointer_type = 0x0f,
+    DW_TAG_reference_type = 0x10,
+    DW_TAG_compile_unit = 0x11,
+    DW_TAG_string_type = 0x12,
+    DW_TAG_structure_type = 0x13,
+    DW_TAG_subroutine_type = 0x15,
+    DW_TAG_typedef = 0x16,
+    DW_TAG_union_type = 0x17,
+    DW_TAG_unspecified_parameters = 0x18,
+    DW_TAG_variant = 0x19,
+    DW_TAG_common_block = 0x1a,
+    DW_TAG_common_inclusion = 0x1b,
+    DW_TAG_inheritance = 0x1c,
+    DW_TAG_inlined_subroutine = 0x1d,
+    DW_TAG_module = 0x1e,
+    DW_TAG_ptr_to_member_type = 0x1f,
+    DW_TAG_set_type = 0x20,
+    DW_TAG_subrange_type = 0x21,
+    DW_TAG_with_stmt = 0x22,
+    DW_TAG_access_declaration = 0x23,
+    DW_TAG_base_type = 0x24,
+    DW_TAG_catch_block = 0x25,
+    DW_TAG_const_type = 0x26,
+    DW_TAG_constant = 0x27,
+    DW_TAG_enumerator = 0x28,
+    DW_TAG_file_type = 0x29,
+    DW_TAG_friend = 0x2a,
+    DW_TAG_namelist = 0x2b,
+    DW_TAG_namelist_item = 0x2c,
+    DW_TAG_packed_type = 0x2d,
+    DW_TAG_subprogram = 0x2e,
+    DW_TAG_template_type_parameter = 0x2f,
+    DW_TAG_template_value_parameter = 0x30,
+    DW_TAG_thrown_type = 0x31,
+    DW_TAG_try_block = 0x32,
+    DW_TAG_variant_part = 0x33,
+    DW_TAG_variable = 0x34,
+    DW_TAG_volatile_type = 0x35,
+    DW_TAG_dwarf_procedure = 0x36,
+    DW_TAG_restrict_type = 0x37,
+    DW_TAG_interface_type = 0x38,
+    DW_TAG_namespace = 0x39,
+    DW_TAG_imported_module = 0x3a,
+    DW_TAG_unspecified_type = 0x3b,
+    DW_TAG_partial_unit = 0x3c,
+    DW_TAG_imported_unit = 0x3d,
+    DW_TAG_condition = 0x3f,
+    DW_TAG_shared_type = 0x40,
+    DW_TAG_type_unit = 0x41,
+    DW_TAG_rvalue_reference_type = 0x42,
+    DW_TAG_template_alias = 0x43,
+    DW_TAG_lo_user = 0x4080,
+    DW_TAG_hi_user = 0xffff,
+}
 
 #[allow(non_camel_case_types)]
 #[repr(u64)]
@@ -713,6 +782,12 @@ impl DIE {
         // attribute not found
         None
     }
+
+    pub fn children<'a>(&self, dwarf: &'a Dwarf) -> DIEChildIterator<'a> {
+        DIEChildIterator {
+            dwarf,
+        }
+    }
 }
 
 #[derive(Clone, Debug)]
@@ -851,16 +926,14 @@ enum ChildIteratorState {
     Finished
 }
 
-struct DIEChildIterator<'a> {
-    compile_unit: &'a CompileUnit,
-    state: ChildIteratorState,
-    cursor: Cursor<'a>,
+pub struct DIEChildIterator<'a> {
+    dwarf: &'a Dwarf,
 }
 
 impl <'a> Iterator for DIEChildIterator<'a> {
     type Item = DIE;
     fn next(&mut self) -> Option<Self::Item> {
-        unimplemented!()
+        return None;
         // match self.state {
         //     ChildIteratorState::Parent(next_pos, ref parent_abbrev) => {
         //         self.cursor.set_position(next_pos);
@@ -1018,6 +1091,14 @@ impl <'a> Iterator for DIEEntryIterator<'a> {
     }
 }
 
+struct DIEIndexEntry {
+    // used to locate DIE compile unit
+    compile_unit_offset: usize,
+
+    // offset of the DIE within .debug_info section
+    die_offset: usize,
+}
+
 pub struct Dwarf {
     elf: Rc<Elf>,
 
@@ -1026,13 +1107,26 @@ pub struct Dwarf {
 
     // keys are offsets within the .debug_abbrev section
     abbrev_tables: HashMap<usize, AbbrevTable>,
+
+    function_index: UnorderedMultiMap<String, DIEIndexEntry>,
 }
 
 impl Dwarf {
     pub fn new(elf: Rc<Elf>) -> Result<Self, Error> {
         let compile_units = Self::parse_compile_units(elf.as_ref())?;
         let abbrev_tables = Self::parse_abbrev_tables(&elf, compile_units.as_ref());
-        Ok(Self { elf, compile_units, abbrev_tables })
+
+        let mut dwarf = Self {
+            elf,
+            compile_units,
+            abbrev_tables,
+            function_index: UnorderedMultiMap::new(),
+        };
+
+        let function_index = dwarf.build_function_index();
+        dwarf.function_index = function_index;
+
+        Ok(dwarf)
     }
 
     fn parse_abbrev_tables(elf: &Elf, compile_units: &[CompileUnit]) -> HashMap<usize, AbbrevTable> {
@@ -1042,6 +1136,43 @@ impl Dwarf {
             abbrev_tables.insert(compile_unit.header.offset, table);
         }
         abbrev_tables
+    }
+
+    fn build_function_index(&self) -> UnorderedMultiMap<String, DIEIndexEntry> {
+        let mut index = UnorderedMultiMap::new();
+
+        for compile_unit in self.compile_units.iter() {
+            if let DIEEntry::Entry(root) = compile_unit.get_root(self) {
+                self.index_die(&root, &mut index);
+            }
+        }
+
+        index
+    }
+
+    fn index_die(&self, die: &DIE, function_index: &mut UnorderedMultiMap<String, DIEIndexEntry>) {
+        let compile_unit = die.get_compile_unit(self);
+        let abbrev = die.get_abbrev(compile_unit, self);
+
+        let has_range = die.get_attribute(abbrev, DwarfAttribute::DW_AT_low_pc as u64).is_some() ||
+            die.get_attribute(abbrev, DwarfAttribute::DW_AT_ranges as u64).is_some();
+
+        if has_range {
+            let is_function = abbrev.tag == DwarfTag::DW_TAG_subprogram as u64 || abbrev.tag == DwarfTag::DW_TAG_inlined_subroutine as u64;
+            if is_function {
+                if let Some(name) = die.name(self) {
+                    let entry = DIEIndexEntry {
+                        compile_unit_offset: compile_unit.header.offset,
+                        die_offset: die.position,
+                    };
+                    function_index.insert(name, entry);
+                }
+            }
+        }
+
+        for child in die.children(self) {
+            self.index_die(&child, function_index);
+        }
     }
 
     pub fn get_compile_units(&self) -> &[CompileUnit] {
